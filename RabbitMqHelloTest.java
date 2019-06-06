2e00fcfe65806dffdebd8e962cbec7822f1b33a1
xufuli <122182953@qq.com>
2019-06-06 16:46:33 +0800

    xufuli22

--- RabbitMqHelloTest.java
+++ RabbitMqHelloTest.java
@@ -2,27 +2,6 @@ package com.dgy.test;
 
 public class RabbitMqHelloTest {
 
-public static void main(String[] args) {
-    System.out.println("！！！！！！！！！！！！！");
 
-    System.out.println("sunping de  push");
-    System.out.println("xuasdhhhhhfuli");
-
-
-	system.out.println("zhangpengxiang1");
-	system.out.println("z");
-	String str = "haha";
-}
-
-
-
-this is  written by penghu 
-this is  written by penghu 2222
-this is  written by penghu 333
-
-
-this is  written by penghu 333
-
-this is  written by penghu 333
 }
 
