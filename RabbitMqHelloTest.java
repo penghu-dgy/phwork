d85f556dd05ce9f1eff157932b5e87bdfaa08934
sunping <351656308@qq.com>
2019-06-06 17:01:30 +0800

    sun6

--- RabbitMqHelloTest.java
+++ RabbitMqHelloTest.java
@@ -3,6 +3,11 @@ package com.dgy.test;
 public class RabbitMqHelloTest {
 
 
+this is written by sunping
 
+this is  written by penghu 
+this is  written by penghu 2222
+this is  written by penghu 333
 
+}
 
